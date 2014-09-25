1c731b19fc251ac8b3e7d4b52c5b41e2b322e7a6
Dan <dans341@gmail.com>
2014-09-25 00:50:37 -0400

    Fixes and additions to JVCL:    fixed flattenLayers as its composite mode was not correct.    added more layer visibility set/get methods     Many more drawing functions including a simple framed box.

--- src/core/JVCL.java
+++ src/core/JVCL.java
@@ -41,9 +41,9 @@ public class JVCL
 		public boolean getVisible()
 			{  return(visible);  }
 		public void setActive(boolean truefalse)
-			{  active = truefalse;   }
+			{  this.active = truefalse;   }
 		public void setVisible(boolean truefalse)
-			{  visible = truefalse;   }
+			{  this.visible = truefalse;   }
 		}
 
 	// Adds a student to the student array list.
@@ -98,19 +98,59 @@ public class JVCL
 		return(true);
 		}
 	
+		// Set layer write to layer X & turn it ON
+		//   then also switches all the other layers OFF
+	public boolean setWriteLayerExclusive( int layerNum )
+		{
+		if( layerNum <= 0 ) { return(false); }
+		if( layerNum >= this.vcl.size() )  { return(false); }
+		this.vcl.get(layerNum).setActive(true);
+		this.vcl.get(layerNum).setVisible(true);
+		this.currentLayer = layerNum;
+		for( int ln = 1; ln < this.vcl.size(); ln++ )
+			{
+			if( ln == layerNum )  { continue; }
+			this.vcl.get(ln).setActive(false);
+			this.vcl.get(ln).setVisible(false);			
+			}
+		return(true);
+		}
+	
 	public int getWriteLayer()
 		{  return( this.currentLayer );  }
 	
-	public boolean setLayerVisibility(int layerNumber ) 
+	public boolean setLayerVisible(int layerNumber ) 
 		{
 		if( layerNumber <= 0 ) { return(false); }
 		if( layerNumber >= this.vcl.size()  )  { return(false); }
 		this.vcl.get(layerNumber).setVisible(true);
 		return(true);
 		}
+	public boolean setLayerInvisible(int layerNumber ) 
+		{
+		if( layerNumber <= 0 ) { return(false); }
+		if( layerNumber >= this.vcl.size()  )  { return(false); }
+		this.vcl.get(layerNumber).setVisible(false);
+		return(true);
+		}
+	
+	public void setLayerVisibility( boolean truefalse )
+		{	this.vcl.get(currentLayer).setVisible(truefalse);	}
 	
 	public boolean getLayerVisibility(int layerNumber)
 		{	return( this.vcl.get(layerNumber).getVisible() );	}
+	
+	public void setLayerVisibilityAll(boolean truefalse ) 
+		{
+		for( int ln = 1; ln < this.vcl.size(); ln++ )
+			{	this.vcl.get(ln).setVisible(truefalse);	}
+		}
+
+	public void setLayerActiveAll(boolean truefalse ) 
+		{
+		for( int ln = 1; ln < this.vcl.size(); ln++ )
+			{	this.vcl.get(ln).setActive(truefalse);	}
+		}
 	
 	private void flattenLayers()
 		{
@@ -129,8 +169,10 @@ public class JVCL
 			g2.setComposite(AlphaComposite.Clear );
 			g2.fillRect(0, 0, this.standardX, this.standardY );
 				// Now set to overlay
-			g2.setComposite(AlphaComposite.Src );
+
 				// And pound down all layers that have drawing turned on.
+				// Sandwich from the top > down, using the right composite mode
+			g2.setComposite(AlphaComposite.SrcOver);
 			for( int ln = 1; ln < this.vcl.size(); ln++ )
 				{
 				if( vcl.get( ln ) == null )   { continue; }
@@ -168,7 +210,7 @@ public class JVCL
 	
 		// Creates a new BufferedImage from any two VCLayers.
 		// returns null if anything went wrong, be sure to check.
-	public BufferedImage joinBufferedImage( 
+	public BufferedImage joinLayersAsBufferedImage( 
 		    int srcLayer1, int srcLayer2 ) 
 		{
 		if( srcLayer1 <= 0 || srcLayer2 <= 0 )   
@@ -188,22 +230,70 @@ public class JVCL
 		}
 
 	
-	public void JVCstring(int x, int y, String s)
+	public boolean JVCstring(int x, int y, String s, Font fnt, Color c )
 		{
-		if( x < 0 || y < 0 ) { return; }
+		if( x < 0 || y < 0 ) { return(false); }
+		
 		Graphics2D g2 = (Graphics2D) vcl.get(this.currentLayer).getImage().getGraphics();
-		g2.setColor( Color.WHITE );
+		g2.setFont( fnt );
+		
+		int checkY = g2.getFontMetrics().getHeight();
+		int checkX = g2.getFontMetrics().stringWidth(s);
+		
+		if( x+checkX > standardX )   
+			{
+			g2.dispose();
+			return(false);
+			}
+		if( y+checkY > standardY )   
+			{
+			g2.dispose();
+			return(false);
+			}
+
+		y += (1 + Math.floorDiv(checkY, 2)); 
+		
+		g2.setColor( c );
 		g2.setComposite(AlphaComposite.Src );
-		g2.setFont( this.nativefont );
 		g2.drawString( s, x, y );
 		g2.dispose();
-		 // anything that writes to layers needs to set this ~ if successful
+		 	// 	anything that writes to layers needs to set this ~ if successful
 		this.requiresUpdate = true; 
+		return(true);
+		}
+
+		// A "dumb" overload form of the above.
+	public boolean JVCstring(int x, int y, String s )
+		{
+		return JVCstring(x,y,s, this.nativefont, Color.white );
+		}
+	
+	public void JVCoval( int cx, int cy, int xRad, int yRad, Color c, boolean fillErIn )
+		{
+		if( cx < 0 || cy < 0 ) { return; }
+		Graphics2D g2 = (Graphics2D) vcl.get(this.currentLayer).getImage().getGraphics();
+		g2.setColor( c );
+		g2.setComposite(AlphaComposite.Src );
+		if( fillErIn == true )
+			{ g2.fillOval( cx-xRad, cy-yRad, xRad*2, yRad*2 ); }
+		else
+			{ g2.drawOval( cx-xRad, cy-yRad, xRad*2, yRad*2 ); }
+		g2.dispose();
+		this.requiresUpdate = true;
 		return;
 		}
 	
-	public void JVCcircle()
+	public void JVCcircle(int cx, int cy, int radius, Color c, boolean fillErIn )
 		{
+		if( cx < 0 || cy < 0 ) { return; }
+		Graphics2D g2 = (Graphics2D) vcl.get(this.currentLayer).getImage().getGraphics();
+		g2.setColor( c );
+		g2.setComposite(AlphaComposite.Src );
+		if( fillErIn == true )
+			{ 	g2.fillOval( cx-radius, cy-radius, radius*2, radius*2 );  }
+		else
+			{	g2.drawOval( cx-radius, cy-radius, radius*2, radius*2 );  }			
+		g2.dispose();
 		this.requiresUpdate = true;
 		return;
 		}
@@ -230,6 +320,20 @@ public class JVCL
 		this.requiresUpdate = true;
 		return;
 		}
+
+	public void JVCrect( int x, int y, int w, int h, Color c )
+		{
+		if( x < 0 || y < 0 ) { return; }
+		if( x+w > this.standardX )  { return; }
+		if( y+h > this.standardY )  { return; }
+		Graphics2D g2 = (Graphics2D) vcl.get(this.currentLayer).getImage().getGraphics();
+		g2.setColor(c);
+		g2.setComposite(AlphaComposite.Src );
+		g2.drawRect( x, y, w, h );
+		g2.dispose();
+		requiresUpdate = true;
+		return;
+		}
 	
 	public void JVCrectfill( int x, int y, int w, int h, Color c )
 		{
@@ -243,6 +347,42 @@ public class JVCL
 		g2.dispose();
 		requiresUpdate = true;
 		return;
+		}
+
+	public boolean JVCmenuPanel( int leftX, int topY, int totalWidth, int totalHeight, Color backgroundColor,
+			int frameWidth, Color frameColor, boolean sunkenFrame )
+		{
+			// minimum possible size for a sunken frame is 3
+		if( (frameWidth < 3) && (sunkenFrame == true) )
+			{ frameWidth = 3; }
+	
+		if( leftX < 0 || topY < 0 ) 				{ return(false); }
+		if( frameWidth*3 > totalWidth ) 	{ return(false); }
+		if( frameWidth*3 > totalHeight ) 	{ return(false); }
+		if( leftX+totalWidth > this.standardX )  { return(false); }
+		if( topY+totalHeight > this.standardY )  { return(false); }
+
+		Graphics2D g2 = (Graphics2D) vcl.get(this.currentLayer).getImage().getGraphics();
+		g2.setColor( backgroundColor );
+		g2.setComposite(AlphaComposite.Src );
+		g2.fillRect( leftX, topY, totalWidth, totalHeight );
+		g2.setColor( frameColor );
+		int fx,fw,fy,fh;
+		for( int a = 0; a < frameWidth; a++ )
+			{
+			if( sunkenFrame == true && (a > 0 && a < (frameWidth - 1) ) )
+				{ continue; }
+			fx = leftX + a;
+			fw = totalWidth - (a*2) - 1;
+			fy = topY + a;
+			fh = totalHeight - (a*2) - 1;
+			
+			g2.drawRect( fx, fy, fw, fh );
+			}
+		
+		g2.dispose();
+		requiresUpdate = true;
+		return(true);
 		}
 	
 	}
