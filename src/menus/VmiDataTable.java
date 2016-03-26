0a6cee973abdfcb5e43e40db50b0569a4185d237
Dan <dans341@gmail.com>
2016-03-26 17:13:00 -0400

    VmiDataTable captions implemented.  Better vertical centering of data elements.

--- src/menus/VmiDataTable.java
+++ src/menus/VmiDataTable.java
@@ -10,17 +10,45 @@ import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
-import core.Controls;
 import domain.VImage;
+
+/**  VmiDataTable - a Vmi element intended for displaying tabular data.
+ * It operates in an "inactive" state, which means Vmenu that attach it
+ * will not allow it to be selected nor will it conform boundries to it.
+ * Any data taken in must be turned into a String type first.
+ * 
+ * Working Freatures :  
+ * 		Background images
+ * 		Dynamic number of columns and rows
+ * 		Image Icon or Text Labels 
+ * 		Optional Caption with a different font
+ * 		Dynamicly changable font.
+ * 		Custom width border and main-block padding
+ * 		Custom (translucent) colors when images are not used.
+ * 		Smart cell clipping and scaling.
+ * 
+ * TODO features:
+ * 		inter-cellular padding.
+ * 
+ * @author Krybo
+ *
+ */
 
 public class VmiDataTable implements Vmenuitem
 	{
+
+	/**  Great resource for font centering.  
+	 * Which is a real nag here.
+	 * http://stackoverflow.com/questions/1055851/how-do-you-draw-a-string-centered-vertically-in-java
+	 */
 	
 	private int x = 0, y = 0, w = 0, h = 0;
 	private int rownum = 0;
 	private int colnum = 0;
 	private Double colMaxW = new Double(0);
 	private Double rowMaxH = new Double(0);
+	private Double captionHeight = new Double(0.0d);
+	private Double captionHeightSave = new Double(0.0d);
 	private int borderWidthPx = 3;
 	private int borderPaddingPx = 10;
 	private boolean active = false;		// not selectable, ever.
@@ -31,7 +59,11 @@ public class VmiDataTable implements Vmenuitem
 	private boolean enableGrid = true;
 	private boolean enableBorder = true;
 	private boolean enableImageBackdrop = false;
-	private String labelTerminator = ":";
+	private boolean enableCaption = false;
+	private String labelTerminator	= new String(":");
+	private String theCaption 		= new String("");
+	private Font fnt = core.Script.fntMASTER;
+	private Font theCaptionFont = this.fnt;
 	
 		// These are required by all menu items.
 	private Long parentID = new Long(-1);
@@ -48,8 +80,6 @@ public class VmiDataTable implements Vmenuitem
 	private HashMap<Integer,Integer> theYpos;
 	private HashMap<Integer,Color> clrSettings;
 
-	private Font fnt = core.Script.fntMASTER;
-	
 	public static enum enumMenuDataTableCOLORS
 		{
 		TEXT_DATA (0),
@@ -91,7 +121,11 @@ public class VmiDataTable implements Vmenuitem
 		this.theXpos		= new HashMap<Integer,Integer>();
 		this.theYpos 		= new HashMap<Integer,Integer>();
 		this.clrSettings 	= new HashMap<Integer,Color>();
-		
+
+		this.theCaption = new String("");
+		this.theCaptionFont = this.fnt;
+		this.enableCaption = false;
+
 		this.active = false;
 		this.visible = true;
 		this.enableImageBackdrop = false;
@@ -129,11 +163,13 @@ public class VmiDataTable implements Vmenuitem
 
 	private void resolvePositions( HashMap<String,String> entries )
 		{
-		int indent = this.borderPaddingPx+this.borderWidthPx; 
+		int indent = this.borderPaddingPx+this.borderWidthPx;
 			// Pixel space to allocate to each column
 		this.colMaxW = new Double( this.w - (2*indent)) / this.colnum;
-		this.rowMaxH = new Double( this.h - (2*indent)) / this.rownum;		
-	
+		this.rowMaxH = new Double( this.h - (2*indent)) / this.rownum;
+
+		this.justifyCaption( 1.2d );
+
 		int td = 1, tr = 1;
 		Integer n = 0;
 		for(  String key : entries.keySet() )
@@ -141,15 +177,15 @@ public class VmiDataTable implements Vmenuitem
 			Double tdw = new Double( td - 1 ) * this.colMaxW;
 	//		Double trw = new Double( tr - 1 ) * this.colMaxW;
 	//		Double tdh = new Double( td - 1 ) * this.rowMaxH;
-			Double trh = new Double( tr - 1 ) * this.rowMaxH;
-	
+			Double trh = new Double( tr - 1 ) * this.rowMaxH + 
+					this.captionHeight;
+
 			this.theData.put( n, entries.get(key) );
 			this.theLabels.put( n, key );
 			this.theXpos.put(n,  tdw.intValue() + indent );
 			this.theYpos.put(n, trh.intValue() + indent );
 	
 			n++;
-			
 				// Layout the table according to scanmode.
 			if( this.scanMode == true )
 				{
@@ -179,23 +215,24 @@ public class VmiDataTable implements Vmenuitem
 		int indent = this.borderPaddingPx+this.borderWidthPx; 
 			// Pixel space to allocate to each column
 		this.colMaxW = new Double( this.w - (2*indent)) / this.colnum;
-		this.rowMaxH = new Double( this.h - (2*indent)) / this.rownum;		
+
+		this.justifyCaption( 1.2d );
 	
 		int td = 1, tr = 1;
-		Integer n = 0;
-		for( Integer keyint : this.theData.keySet() )
+		
+//		for( Integer keyint : this.theData.keySet() )
+		for( Integer n = 0;  n < this.theData.size(); n++ )
 			{
-			String key = this.theLabels.get(keyint);
+//			String key = this.theLabels.get(keyint);
 			Double tdw = new Double( td - 1 ) * this.colMaxW;
-			Double trh = new Double( tr - 1 ) * this.rowMaxH;
+			Double trh = new Double( tr - 1 ) * this.rowMaxH +
+					this.captionHeight;
 	
 //			System.out.println("DEBUG  tdw = "+tdw.toString());
 			
 			this.theXpos.put(n,  tdw.intValue() + indent );
 			this.theYpos.put(n, trh.intValue() + indent );
 	
-			n++;
-			
 				// Layout the table according to scanmode.
 			if( this.scanMode == true )
 				{
@@ -220,6 +257,30 @@ public class VmiDataTable implements Vmenuitem
 		}
 
 
+	// Adjusts the vertical positioning associated with introducing a caption
+	//  Factor is a Double that scales the height of the caption.
+	private void justifyCaption( Double factor )
+		{
+		if( ! this.enableCaption )  { return; }
+		this.rowMaxH = new Double( this.h - 
+			(2*this.borderPaddingPx+this.borderWidthPx)) / this.rownum;
+
+		if( this.enableCaption == true && ! this.theCaption.isEmpty() )
+			{
+			Double indent = new Double(
+				2*this.borderPaddingPx+this.borderWidthPx );
+			// Do not calculate this unless we need to.
+			if( this.captionHeight <= 0.0d )
+				{
+				this.captionHeight = new Double(this.rowMaxH * factor );
+				}
+			// The caption reduces space available for the normal table cells.
+			this.rowMaxH = 
+				(new Double( this.h ) - indent - this.captionHeight) / 
+				new Double( this.rownum );
+			}
+		return;
+		}
 	
 	public boolean reposition(int posX, int posY, int relPosX, int relPosY)
 		{
@@ -249,16 +310,67 @@ public class VmiDataTable implements Vmenuitem
 		int x0 = this.x;
 		int y0 = this.y;
 		int x1 = x0 + this.borderWidthPx + this.borderPaddingPx;
-		int y1 = y0 + this.borderWidthPx + this.borderPaddingPx;
+		int y1 = y0 + this.borderWidthPx + this.borderPaddingPx +
+				this.captionHeight.intValue();
 		int x2 = x0 + this.w;
 		int y2 = y0 + this.h;
 		int xw = x2 - this.borderWidthPx - this.borderPaddingPx;
-		int yh = y2 - this.borderWidthPx - this.borderPaddingPx;
+//		int yh = y2 - this.borderWidthPx - this.borderPaddingPx;
 		Double tmp;
+
+		FontMetrics fm =
+				target.getImage().getGraphics().getFontMetrics(
+				this.fnt );
+		FontRenderContext frc = fm.getFontRenderContext();
 
 			// recalculate this in case margins have changed.
 		this.colMaxW = new Double( xw - x1) / this.colnum;
-		this.rowMaxH = new Double( yh - y1) / this.rownum;
+		this.justifyCaption( 1.2d );
+
+		Double captionStartX = new Double(x1);
+		Double captionStartY = new Double( 0.0d );
+		if( this.enableCaption == true && ! this.theCaption.isEmpty() )
+			{
+			FontMetrics fm2 =
+					target.getImage().getGraphics().getFontMetrics(
+					this.theCaptionFont );
+			FontRenderContext frc2 = fm2.getFontRenderContext();
+			
+			this.captionHeight =
+				Math.abs( (this.theCaptionFont.getStringBounds(
+						this.theCaption, frc2 ).getY() +
+				this.theCaptionFont.getStringBounds(
+						this.theCaption, frc2 ).getMinY() )  );
+
+				// Only do this if a change is detected.
+			if( this.captionHeight != this.captionHeightSave )
+				{
+				this.resolvePositions();
+				this.captionHeightSave = this.captionHeight;
+				}
+
+			captionStartX = 
+					new Double( this.theCaptionFont.getStringBounds(
+					this.theCaption, frc2 ).getWidth() );
+			captionStartX = (this.w - captionStartX ) / 2.0d;
+			captionStartY = new Double( y0 + this.borderWidthPx + 
+					this.borderPaddingPx) +
+					(this.captionHeight / 2.0d);
+//					new Double( this.theCaptionFont.getStringBounds(
+//					this.theCaption, frc2 ).getHeight() ) / 2.0d ;
+//			captionStartX = ( (new Double(this.w) / 2.0d) - 
+//					(captionStartX / 2.0d ) );
+			y1 = y0 + this.borderWidthPx + this.borderPaddingPx +
+					this.captionHeight.intValue();
+			}
+		else if( this.captionHeight != 0.0d )
+			{
+			this.captionHeight = 0.0d;
+			captionStartX = 0.0d;
+			captionStartY = 0.0d;
+			y1 = y0 + this.borderWidthPx + this.borderPaddingPx;
+			this.resolvePositions();
+			}
 
 			// Background bounding box (or image)
 		if( this.enableImageBackdrop == true && this.bkgImg != null )
@@ -278,17 +390,18 @@ public class VmiDataTable implements Vmenuitem
 			{
 
 			for( double a = 1.0d; a < this.colnum; a += 1.0d )
-				{
+				{		// VERTICAL
 				tmp = a * this.colMaxW;
 //				System.out.println(" Column "+Double.toString(a)+ " = "+tmp.toString() 
 //						+ " { " + this.colMaxW.toString() + " } " );
-				target.line( x1+tmp.intValue(), y0, 
+				target.line( x1+tmp.intValue(), 
+					y0+this.captionHeight.intValue(), 
 					x1+tmp.intValue(), y2, this.clrSettings.get(
 					enumMenuDataTableCOLORS.GRIDLINES_Y.value()));
 				}
 			for( double b = 1.0d; b < this.rownum; b += 1.0d )
-				{
-				tmp = b * this.rowMaxH;
+				{		// HORIZONTAL
+				tmp = b * this.rowMaxH;		// row descent.
 //				System.out.println(" Row "+Double.toString(b)+ " = "+tmp.toString()
 //						+ " { " + this.rowMaxH.toString() + " } " );
 				target.line( x0, y1+tmp.intValue(), x2, 
@@ -315,18 +428,28 @@ public class VmiDataTable implements Vmenuitem
 					}
 				if( fc == null )  {  fc = Color.WHITE; } 
 				target.rect( x0+fn, y0+fn, x2 - fn, y2 - fn, fc );
+				if( this.enableCaption == true && ! this.theCaption.isEmpty() )
+					{
+					target.rect( x0+fn, y0+fn,	x2 - fn, 
+						y0 + this.captionHeight.intValue() - fn, fc );	
+					}
 				}
 			}
-		
+
 //		System.out.println( " Data # " + Integer.toString( this.theData.size() ) );
 
-		FontMetrics fm =
-				target.getImage().getGraphics().getFontMetrics(
-				this.fnt );
-		FontRenderContext frc = fm.getFontRenderContext();
+		// Write the Caption.
+		if( this.enableCaption == true && ! this.theCaption.isEmpty() )
+			{
+			target.printString( captionStartX.intValue(),  captionStartY.intValue(),  
+				this.theCaptionFont, 
+				this.clrSettings.get(
+					enumMenuDataTableCOLORS.TEXT_DATA.value() ),
+				this.theCaption	);
+			}
 
 		// Finally, print all the labels & values.
-		String label = new String("");
+//		String label = new String("");
 		String tmpLabel = new String("");
 		tmpLabel.trim();
 		String tmpData = new String("");
@@ -336,16 +459,25 @@ public class VmiDataTable implements Vmenuitem
 			{
 			tmpData = this.theData.get(s);
 			tmpLabel = this.theLabels.get(s)+this.labelTerminator;
-
+			
 			int dw0 = new Double( this.fnt.getStringBounds(
 					tmpData, frc ).getWidth() ).intValue();
-			int dy0 = new Double( this.fnt.getStringBounds(
-					tmpData, frc ).getMaxY() ).intValue();
+//			int dy0 = new Double( this.fnt.getStringBounds(
+//					tmpData, frc ).getMaxY() ).intValue();
 			int dy1 = new Double( this.fnt.getStringBounds(
 					tmpData, frc ).getMinY() ).intValue();
-			int dy2 = new Double( this.fnt.getStringBounds(
-					tmpData, frc ).getHeight() ).intValue();
+//			int dy2 = new Double( this.fnt.getStringBounds(
+//					tmpData, frc ).getHeight() ).intValue();
 
+			double dy3 = this.fnt.getStringBounds( tmpData, frc ).getY();
+			// half of unutilized space
+//			int dy4 = new Double((this.rowMaxH + dy3) / 2.0d ).intValue();
+			int dy4 = new Double( Math.abs(dy3) / 2.0d ).intValue();
+			
+//			int dy3 = 	Math.abs( new Double( 
+//		this.fnt.getStringBounds( tmpData, frc ).getY() +
+//		this.fnt.getStringBounds( tmpData, frc ).getMinY() ).intValue() )+1;
+			
 			int shorten = dw0;
 				// This thing is longer then the space allocated.
 				//   will need to truncate it.
@@ -361,8 +493,11 @@ public class VmiDataTable implements Vmenuitem
 			// note: indentation is built into : this.theXpos.get(s) 			
 			int Xadj = x0 + this.theXpos.get(s).intValue() 
 					+ this.colMaxW.intValue() - shorten - 1;
+			// Bumps text baseline down to vertical center of cell 
 			int Yadj = y0 + this.theYpos.get(s).intValue()
-					+ (this.rowMaxH.intValue() / 2) + dy0;
+					+ (this.rowMaxH.intValue() / 2) + dy4;
+//					+ (dy3 / 2);
+//					+ (this.rowMaxH.intValue() / 2) + dy0;
 
 			if( this.useLabels == true )
 				{
@@ -377,10 +512,10 @@ public class VmiDataTable implements Vmenuitem
 					Double scFct = new Double(
 						this.imgLabels.get(s).height) / (this.rowMaxH-4.0d);
 					target.scaleblit( 2 + x0 + this.theXpos.get(s).intValue(), 
-							2 + y0 + this.theYpos.get(s).intValue(), 
-							new Double(this.imgLabels.get(s).width / scFct).intValue(), 
-							this.rowMaxH.intValue() - 4, 
-							this.imgLabels.get(s) );
+						2 + y0 + this.theYpos.get(s).intValue(), 
+						new Double(this.imgLabels.get(s).width / scFct).intValue(), 
+						this.rowMaxH.intValue() - 4, 
+						this.imgLabels.get(s) );
 					}
 				else
 					{
@@ -672,10 +807,34 @@ public class VmiDataTable implements Vmenuitem
 		this.resolvePositions();
 		return(true);
 		}
-
-	// TODO:  Captions.
 	
+	public void enableCaption( boolean onOff )
+		{
+		this.enableCaption = onOff;
+		return;
+		}
+
+	public boolean toggleCaption()
+		{
+		if( this.theCaption.isEmpty() )  { return(this.enableCaption); }
+		this.enableCaption = ! this.enableCaption;
+		return(this.enableCaption);
+		}
+
+	public void setCaption(String caption, Font capFont, boolean enable)
+		{
+		this.theCaption = caption;
+		this.theCaptionFont = capFont;
+		this.enableCaption(enable);
+		this.resolvePositions();
+		return;
+		}
+	
+
+	//  TODO : mop up handling of "inactive" menuitems in VmenuVertical
+	// TODO : add more versatile constructors to this class.
 	
 	}			// END class  VmiDataTable.
+
 
 
